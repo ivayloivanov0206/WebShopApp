236231263a45d22d201a60af8ec545f3f8fcebdd
ivayloivanov0206'git config --global user.email i.ivanov2020@pmg-pernik.comgit add .git commit -m Upload <i.ivnaov2020@pmg-pernik.com>
2024-12-11 09:02:14 +0200

    Update WebShopApp

---
+++ WebShopApp.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+using WebShopApp.Infrastructure.Data.Domain;
+
+namespace WebShopApp.Intrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
