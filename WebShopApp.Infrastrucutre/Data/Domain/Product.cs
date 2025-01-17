592ca563e7dd2ccc92cfd8da7643ab947da84935
ivayloivanov0206'git config --global user.email i.ivanov2020@pmg-pernik.comgit add .git commit -m Upload <i.ivnaov2020@pmg-pernik.com>
2025-01-17 11:05:59 +0200

    updated webshop

--- WebShopApp.Infrastrucutre/Data/Domain/Product.cs
+++ WebShopApp.Infrastrucutre/Data/Domain/Product.cs
@@ -14,8 +14,8 @@ namespace WebShopApp.Infrastrucutre.Data.Domain
         [MaxLength(30)]
         public string ProductName { get; set; }
         [Required]
-        public int BrandId {  get; set; }
-        public virtual Category Brand { get; set; } = null!;
+        public int BrandId { get; set; }
+        public virtual Brand Brand { get; set; } = null!;
         [Required]
         public int CategoryId {  get; set; }
         public virtual Category Category { get; set; } = null!;
