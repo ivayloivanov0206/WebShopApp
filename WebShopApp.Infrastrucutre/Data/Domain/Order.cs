07c0334e2d822bd229c0f6775d01a66026ced6cc
ivayloivanov0206 <i.ivanov2020@pmg-pernik.com>
2025-01-21 11:34:55 +0200

    updated webshop

--- WebShopApp.Infrastrucutre/Data/Domain/Order.cs
+++ WebShopApp.Infrastrucutre/Data/Domain/Order.cs
@@ -14,10 +14,10 @@ namespace WebShopApp.Infrastrucutre.Data.Domain
         
         public DateTime OrderDate { get; set; }
         [Required]
-        public int ProducctId { get; set; }
+        public int ProductId { get; set; }
         public virtual Product Product { get; set; } = null!;
         [Required]
-        public int UserId { get; set; }
+        public string UserId { get; set; }
         public virtual ApplicationUser User { get; set; } = null!;
         public int Quantity { get; set; }
         public decimal Price { get; set; }
